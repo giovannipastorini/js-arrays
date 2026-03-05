3a4260f99af9e0dc380c4b735461e8be9d1ee748
giovannipastorini <giovannipastorini9@gmail.com>
2026-03-05 05:22:57 +0100

    update value in arrray

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -10,9 +10,17 @@ const teachers = [
 
 // 1. Utilizzando un indice sull'array teachers, cambia il valore della
 // variabile fourthTeacher qui sotto in modo che sia il quarto insegnante elencato nell'array
-const fourthTeacher = null;
+const fourthTeacher = teachers[3];
+
+console.log("Questo è il quarto insegnante della lista: "+fourthTeacher);
+
 
 // 2. Sostituisci il quinto insegnante nell'array teachers con 'Patrick'
+console.log("Quinto insegnante: "+teachers[4]);
+
+teachers[4]="Patrick";
+
+console.log("Quinto insegnante aggiornato: "+teachers[4]);
 
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
 const lastTeacher = null;
