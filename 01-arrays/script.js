85a6b4c608b3b12043c607ef833e809368f06875
giovannipastorini <giovannipastorini9@gmail.com>
2026-03-06 17:17:42 +0100

    task 8: using length property to examine the array

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -73,4 +73,11 @@ console.log(lewisIndex);
 
 // 8. Verifica se l'array teachers è vuoto
 // e salva il risultato nella variabile isTeachersEmpty
-const isTeachersEmpty = null;
+
+let isTeachersEmpty = false;
+
+if (teachers.length === 0){
+  isTeachersEmpty= true;
+}
+
+console.log(isTeachersEmpty);
